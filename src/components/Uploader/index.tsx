a8eddc67e82f6ff255df79d949d4e55330d4cac9
Harmeet Singh <harmeet@msgitwingtech.com>
2025-06-02 10:53:16 +0530

    refactor: replace custom typography with TextField for better file input handling and also displayed comma seprated file names

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -1,4 +1,4 @@
-import { FormHelperText, Typography, useTheme } from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 import ImageIcon from "@mui/icons-material/Image";
 import { IFileUploader } from "../../types";
 import UploaderModal from "./UploaderModal";
@@ -15,7 +15,6 @@ const FileUploader = ({
   onDeleteFile,
   ...rest
 }: IFileUploader.Props) => {
-  const theme = useTheme();
   const [showModal, setShowModal] = useState(false);
   const handleCloseModal = () => {
     stopStreamedVideo();
@@ -27,36 +26,37 @@ const FileUploader = ({
   return (
     <Typography component={"div"} sx={{ width: "100%" }}>
       {/* visible component */}
-      <Typography
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: 0.5,
-          border: !error
-            ? "1px solid rgba(0, 0, 0, 0.2)"
-            : `1px solid ${theme.palette.error.main}`,
-          "border:hover": "1px solid rgba(0, 0, 0, 0.87)",
-          borderRadius: "4px",
-          cursor: "pointer",
-          padding: size === "small" ? 1 : 1.8,
-        }}
-        onClick={() => setShowModal(true)}
-      >
-        <ImageIcon color={error ? "error" : "inherit"} fontSize="medium" />
-        <Typography
-          component={"span"}
-          sx={{
-            color: error
-              ? theme.palette.error.main
-              : theme.palette.text.primary,
-          }}
-        >
-          {label} {count && `(${files.length})`}
-        </Typography>
-      </Typography>
 
-      {/* error */}
-      {!!error && <FormHelperText error={!!error}>{error}</FormHelperText>}
+      <TextField
+        label={`${label} ${count && `(${files.length})`}`}
+        size="small"
+        variant="outlined"
+        value={files.map((file) => file.name).join(", ")}
+        onClick={() => setShowModal(true)}
+        fullWidth
+        error={!!error}
+        helperText={error}
+        slotProps={{
+          inputLabel: {
+            shrink: !!files.length,
+            sx: { ml: files.length ? 0 : 3 },
+          },
+          input: {
+            sx: {
+              pr: 0.5,
+              pl: 1,
+            },
+            readOnly: true,
+            startAdornment: (
+              <ImageIcon
+                color={error ? "error" : "inherit"}
+                fontSize="medium"
+                sx={{ pr: 0, mr: 0.5 }}
+              />
+            ),
+          },
+        }}
+      />
 
       {/* file uplodaer modal */}
       {showModal && (
